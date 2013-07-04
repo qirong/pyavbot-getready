39dfaf9f2942dd3a1622100f6bc96891f2a85705
coleman <omegacoleman@gmail.com>
2013-07-04 13:18:25 +0800

    加入web.py的server，可在线察看log。

--- lwqq-hi.py
+++ lwqq-hi.py
@@ -1,20 +1,33 @@
 ﻿#-*- coding=utf-8 -*-
 
+import os
+import sys
+curr_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(curr_path + os.sep + 'webpy')
+import web
 from webqq import WebQQ
 from daemon import Daemon
 from os import name as osname
 import getpass
 import time
-import sys
-import os
+import threading
+from cgi import escape as htmlescape
+
+LOG_PATH = curr_path + os.sep + 'qqbot_log'
+ERR_PATH = curr_path + os.sep + 'qqbot_log_e'
+TMPL_PATH = curr_path + os.sep + 'templates'
+
+f_log = None
+
+urls = (
+    '/', 'log'
+)
 
 def reply_qq(sender_id, sender, msg):
+    global f_log
     msg = msg.strip()
-    if osname == 'nt':
-        print sender.decode("utf-8").encode("gbk"), " : ", \
-        msg.decode("utf-8").encode("gbk")
-    else:
-        print sender, " : ", msg
+    f_log.write("<p>" + htmlescape(sender) + " : " + htmlescape(msg) + "</p>")
+    f_log.flush()
     if msg == "醒醒，醒醒":
         if sender != "":
             return sender + "，来啦来啦~"
@@ -31,6 +44,11 @@ class MyDaemon(Daemon):
         self.qq.on_gotmsg = reply_qq
         self.qq.login()
     def run(self):
+        global f_log
+        f_log = open(LOG_PATH, 'a+')
+        t = threading.Thread(target = webserver)
+        t.daemon = True
+        t.start()
         while 1:
             try:
                 self.qq.step()
@@ -40,23 +58,41 @@ class MyDaemon(Daemon):
             except Exception as e:
                 print "Error : ", e
 
+def webserver():
+    app = web.application(urls, globals())
+    app.run()
+
+class log:
+    def GET(self):
+        web.header('Content-type', "text/html; charset=utf-8")
+        f = open(LOG_PATH)
+        s = f.read()
+        f.close()
+        render = web.template.render(TMPL_PATH)
+        return render.log(s)
+
 if __name__ == "__main__":
-    daemon = MyDaemon('/tmp/daemon-qqbot.pid', stdout=(os.getcwd() + os.sep + 'qqbot_log'))
+    daemon = MyDaemon('/tmp/daemon-qqbot.pid', 
+    stdout = ERR_PATH, 
+    stderr = ERR_PATH)
     if osname == 'nt':
         daemon.qq_init()
         daemon.run()
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.qq_init()
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
     else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
+        if len(sys.argv) >= 2:
+            fa = sys.argv[1]
+            sys.argv.remove(fa)
+            if 'start' == fa:
+                daemon.qq_init()
+                daemon.start()
+            elif 'stop' == fa:
+                daemon.stop()
+            elif 'restart' == fa:
+                daemon.restart()
+            else:
+                print "Unknown command"
+                sys.exit(2)
+            sys.exit(0)
+        else:
+            print "usage: %s start|stop|restart" % sys.argv[0]
+            sys.exit(2)
